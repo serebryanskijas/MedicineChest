d2138cbe1bd31c9abeb9ba012cfd92e678eeef50
alexs <a.serebryansky@isida.by>
2022-08-17 16:31:00 +0300

    17.08

--- src/main/java/MedicineChest/medicineChest/MedicineChestController.java
+++ src/main/java/MedicineChest/medicineChest/MedicineChestController.java
@@ -1,11 +1,15 @@
 package MedicineChest.medicineChest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.util.StringUtils;
 
 import javax.servlet.http.HttpServletResponse;
+
 
 @Controller
 public class MedicineChestController {
@@ -13,31 +17,36 @@ public class MedicineChestController {
     @Autowired
     private MedicineChestService medicineChestService;
 
-    @GetMapping(value ="/add_medicineChest")
-    public String index(Model model) {
-        model.addAttribute("medicineChest",new MedicineChest()); //Если не добавить, то не будет выполняться парсинг шаблона исходной страницы
-        return "add_medicineChest";
-    }
+    private final Logger logger = LoggerFactory.getLogger(MedicineChestController.class);;
 
-    @GetMapping(value ="/medicineChests" )
+    @GetMapping(value = "/medicineChests")
     public String listMedicineChest(Model model) {
         model.addAttribute("medicineChests", medicineChestService.findAll());
         return "medicineChests";
     }
 
-    @PostMapping(value="/add_medicineChest")
+    @GetMapping(value = "/add_medicineChest")
+    public String index(Model model) {
+        model.addAttribute("medicineChest",
+                new MedicineChest()); //Если не добавить, то не будет выполняться парсинг шаблона исходной страницы
+        return "add_medicineChest";
+    }
+
+    @PostMapping(value = "/add_medicineChest")
     public String saveMedicineChest(MedicineChest medicineChest, Model model, HttpServletResponse response) {
         System.out.println(medicineChest);
         //Передать id в заголовке ответа
-        MedicineChest newMedicineChest = medicineChestService.save(medicineChest);
-        long id = newMedicineChest.getId();
-        response.addHeader("id", String.valueOf(id));
+        if (!StringUtils.isEmpty(medicineChest.getName())) {
+            MedicineChest newMedicineChest = medicineChestService.save(medicineChest);
+            long id = newMedicineChest.getId();
+            response.addHeader("id", String.valueOf(id));
+        }
         model.addAttribute("medicineChests", medicineChestService.findAll());
         return "redirect:/medicineChests";
     }
 
     @GetMapping(value = "/delete_medicineChest")
-    public String deleteMedicineChest(@RequestParam(name="id")Long id) {
+    public String deleteMedicineChest(@RequestParam(name = "id") Long id) {
         medicineChestService.deleteById(id);
         return "redirect:/medicineChests";
     }
@@ -51,17 +60,21 @@ public class MedicineChestController {
         return "redirect:/edit_medicineChest?id=" + chestId;
     }*/
 
-
-
-    @GetMapping(value ="/edit_medicineChest")
-    public String editMedicineChest(Model model, @RequestParam(name="id")Long id) {
+    @GetMapping(value = "/edit_medicineChest")
+    public String editMedicineChest(Model model, @RequestParam(name = "id") Long id) {
         MedicineChest medicineChest = medicineChestService.findById(id);
-
-        model.addAttribute("medicines",medicineChest);
+        model.addAttribute("medicines", medicineChest);
         return "edit_medicineChest";
     }
 
-    @PutMapping(value="/update_medicineChest")
+    @GetMapping(value = "/update_medicineChest")
+    public String updateMedicineChest(Model model, @RequestParam(name = "id") Long id) {
+        MedicineChest medicineChest = medicineChestService.findById(id);
+        model.addAttribute("medicineChests", medicineChest);
+        return "update_medicineChest";
+    }
+
+    @PostMapping(value = "/update_medicineChest")
     public String updateMedicineChest(MedicineChest medicineChest, Model model) {
         MedicineChest medicineChestDb = medicineChestService.findById(medicineChest.getId());
         medicineChestDb.setName(medicineChest.getName());
@@ -69,4 +82,5 @@ public class MedicineChestController {
         model.addAttribute("medicineChests", medicineChestService.findAll());
         return "redirect:/medicineChests";
     }
+
 }
